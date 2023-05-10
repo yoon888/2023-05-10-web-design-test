4f26a3fa7264c54863ae4e273ca634f139db4591
yoon88 <ysmdudwo@hotmail.com>
2023-05-10 16:58:44 +0900

    test

---
+++ script/common.js
@@ -0,0 +1,38 @@
+$(function(){
+    //메뉴
+    $('.gnb').hover(
+        function(){
+            $('.gnb ul').stop().slideDown('fast')
+        },
+        function(){
+            $('.gnb ul').stop().slideUp('fast')
+        }
+    )
+
+    //슬라이드
+    setInterval(function(){
+        $('.slide').animate({top:-300},function(){
+            $('.slide a:first').appendTo($('.slide'))
+            $('.slide').css('top',0)
+        })
+    },3000)
+
+    //탭
+    $('.tab_title h2').click(function(){
+        $('.tab_title h2').removeClass('on')
+        $(this).addClass('on')
+
+        $('.tab_contents ul').css('display','none')
+        $('.tab_contents ul').eq($(this).index()).css('display','block')
+    })
+
+    //모달
+    $('.notice li:first').click(function(){
+        $('.modal').addClass('active')
+    })
+
+    $('.btn').click(function(){
+        $('.modal').removeClass('active')
+    })
+
+})
